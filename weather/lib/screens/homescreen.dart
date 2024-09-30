aa363cba099f02fde38fa481e51cbddfdaae70ab
Jawwad Ahmed <mjawwadahmed2021@gmail.com>
2024-10-01 00:11:52 +0500

    updated

--- weather/lib/screens/homescreen.dart
+++ weather/lib/screens/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -12,7 +13,7 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   TextEditingController search = TextEditingController();
-  String city = "";
+  String city = "Karachi";
   String weather = "";
   num? windSpeed;
   num? temp;
@@ -26,10 +27,16 @@ class _HomescreenState extends State<Homescreen> {
     return temp != null ? (temp! * 9 / 5) + 32 : 0;
   }
 
+  @override
+  void initState() {
+    super.initState();
+    // Fetch weather data for the default city on app launch
+    getweather();
+  }
+
   void getweather() async {
-    String searchCity = search.text;
     var data = await http.get(Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$searchCity&appid=5bd7a27bebc6db6a9dfdc53748a3bbc2&units=metric"));
+        "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=ur api key&units=metric"));
 
     if (data.statusCode == 200) {
       var response = jsonDecode(data.body);
@@ -61,99 +68,98 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      appBar: AppBar(
+        title: Center(
+            child: Text('Weather App',
+                style: GoogleFonts.poppins(
+                    fontSize: 24, fontWeight: FontWeight.w700))),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10),
+                Row(
                   children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: search,
-                            decoration: InputDecoration(
-                              hintText: "Search city,state or country",
-                              prefixIcon: const Icon(Icons.search),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30)),
-                            ),
-                            onSubmitted: (value) {
-                              if (value.isNotEmpty) {
-                                getweather();
-                              }
-                            },
+                    Expanded(
+                      child: TextField(
+                        controller: search,
+                        decoration: InputDecoration(
+                          hintText: "Search city or country",
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
                           ),
                         ),
-                        TextButton(
-                            onPressed: () {
-                              getweather();
-                            },
-                            child: const Text(
-                              "Search",
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.black),
-                            ))
-                      ],
+                        onSubmitted: (value) {
+                          if (value.isNotEmpty) {
+                            city = value;
+                            getweather();
+                          }
+                        },
+                      ),
                     ),
-                    const SizedBox(
-                      height: 20,
+                    TextButton(
+                      onPressed: () {
+                        if (search.text.isNotEmpty) {
+                          city = search.text;
+                          getweather();
+                        }
+                      },
+                      child: const Text(
+                        "Search",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
                     ),
-                    Icon(
-                      temp != null ? Icons.cloud : Icons.error,
-                      size: 80,
-                      color: Colors.blue,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(city, style: const TextStyle(fontSize: 24)),
-                    Text(temp != null ? '$temp °C' : 'N/A',
-                        style: const TextStyle(fontSize: 48)),
-                    Text(fah != null ? '$fah °F' : 'N/A',
-                        style: const TextStyle(fontSize: 24)),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(temp != null ? formattedDate : "",
-                        style: const TextStyle(color: Colors.grey)),
-                    Text(weather, style: const TextStyle(fontSize: 24)),
-                    const SizedBox(height: 20),
-                    const Divider(thickness: 2),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        _buildWeatherInfo(Icons.air, "${windSpeed ?? 0} km/h",
-                            "Wind", Colors.black),
-                        _buildWeatherInfo(Icons.cloud, "${cloud ?? 0} %",
-                            "Cloud", Colors.blue),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        _buildWeatherInfo(Icons.speed, "${pressure ?? 0} mb",
-                            "Pressure", Colors.black),
-                        _buildWeatherInfo(Icons.opacity, "${humidity ?? 0} %",
-                            "Humidity", Colors.blue),
-                      ],
-                    )
                   ],
                 ),
-              )),
-        ));
+                const SizedBox(height: 20),
+                const Icon(
+                  Icons.cloud,
+                  size: 80,
+                  color: Colors.blue,
+                ),
+                const SizedBox(height: 10),
+                Text(city, style: const TextStyle(fontSize: 24)),
+                Text('$temp °C', style: const TextStyle(fontSize: 48)),
+                Text('$fah °F', style: const TextStyle(fontSize: 24)),
+                const SizedBox(height: 10),
+                Text(formattedDate, style: const TextStyle(color: Colors.grey)),
+                Text(weather, style: const TextStyle(fontSize: 24)),
+                const SizedBox(height: 20),
+                const Divider(thickness: 2),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildWeatherInfo(
+                        Icons.air, "$windSpeed km/h", "Wind", Colors.black),
+                    _buildWeatherInfo(
+                        Icons.cloud, "$cloud %", "Cloud", Colors.blue),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildWeatherInfo(
+                        Icons.speed, "$pressure mb", "Pressure", Colors.black),
+                    _buildWeatherInfo(
+                        Icons.opacity, "$humidity %", "Humidity", Colors.blue),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildWeatherInfo(
--- weather/lib/screens/splash.dart
+++ weather/lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather/screens/homescreen.dart';
 import 'dart:async';
 
@@ -23,11 +24,17 @@ class _SplashState extends State<Splash> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/img.png'), fit: BoxFit.cover),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('TempTrack',
+                style: GoogleFonts.poppins(
+                    fontSize: 24, fontWeight: FontWeight.w700)),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
+          ],
         ),
       ),
     );
